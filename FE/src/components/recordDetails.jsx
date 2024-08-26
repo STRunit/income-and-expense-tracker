ff2cffdbe691cb67f5b0cb122d678be7ea5a9f14
STRunit <st21unit@gmail.com>
2024-08-26 03:10:56 -0700

    Add Records page styles

--- FE/src/components/recordDetails.jsx
+++ FE/src/components/recordDetails.jsx
@@ -2,15 +2,9 @@ import { LeftIcon } from "./icon/leftIcon"
 import { RightIcon } from "./icon/rightIcon"
 
 export const RecordDetails = () => {
-    return <div className="mt-6">
-        <div>
-            <div className="inline-flex items-center gap-4">
-                <LeftIcon className="bg-[#E5E7EB] p-1 rounded-md"/>
-                <p>Last 30 Days</p>
-                <RightIcon className="bg-[#E5E7EB] p-1 rounded-md"/>
-            </div>
-            <div>select</div>
-        </div>
-        <div></div>
+    return <div className="flex h-fit gap-4">
+        <LeftIcon className="bg-[#E5E7EB] p-1 rounded-md" />
+        <p>Last 30 Days</p>
+        <RightIcon className="bg-[#E5E7EB] p-1 rounded-md" />
     </div>
 }
